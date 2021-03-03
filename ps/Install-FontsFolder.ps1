7173b318a5f95fd1a2a7c4ec5d6139afdde50eb6
Dailen Gunter <me@dg4.dev>
2021-03-03 13:52:11 -0500

    Added file type in font name

--- ps/Install-FontsFolder.ps1
+++ ps/Install-FontsFolder.ps1
@@ -5,14 +5,37 @@ param(
 $regPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts"
 Add-Type -AssemblyName PresentationCore
 
+$hashFontFileTypes = @{}
+$hashFontFileTypes.Add(".fon", "")
+$hashFontFileTypes.Add(".fnt", "")
+$hashFontFileTypes.Add(".ttf", " (TrueType)")
+$hashFontFileTypes.Add(".ttc", " (TrueType)")
+$hashFontFileTypes.Add(".otf", " (OpenType)")
+
 foreach ($font in (Get-ChildItem "$fontFolder"))
 {
-        $fontName = (New-Object -TypeName Windows.Media.GlyphTypeface -ArgumentList $font.VersionInfo.FileName).Win32FamilyNames.Values
+        $fontName = (New-Object -TypeName Windows.Media.GlyphTypeface -ArgumentList $font.FullName).Win32FamilyNames.Values
+	$fontExt = $font.Extension
         $regKeyName = $fontName -join " "
         $regKeyValue = $font.Name
 
         Copy-Item $font "C:\Windows\Fonts" -Force -ErrorAction 'silentlycontinue'
-	
-        New-ItemProperty -Path $regPath -Name $regKeyName -Value $regKeyValue -Force -ErrorAction 'silentlycontinue'
 
+        if (-not($hashFontFileTypes.ContainsKey($fontExt))) 
+	{
+		Write-Host "File Extension Unsupported"
+		$retVal = 0
+	}
+
+        if ($retVal -eq 0)
+	{
+            Write-Host "Font $($font.FullName) installation failed." -ForegroundColor Red
+            Write-Host ""
+            1
+        }
+        else
+        {
+            New-ItemProperty -Path $regPath -Name "$($fontName)$($hashFontFileTypes.$fontExt)" -Value $regKeyValue -Force -ErrorAction 'silentlycontinue'
+            Write-Host "Font $($font.FullName) $($hashFontFileTypes.$fontExt) installed successfully." -ForegroundColor Green
+        }
 }
